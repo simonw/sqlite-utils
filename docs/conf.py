463fd3c3f66691532022f14ad446f9b782d2273c
Simon Willison <swillison@gmail.com>
2019-02-22 22:08:14 -0800

    Bump copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "sqlite-utils"
-copyright = "2018, Simon Willison"
+copyright = "2019, Simon Willison"
 author = "Simon Willison"
 
 # The version info for the project you're documenting, acts as replacement for
