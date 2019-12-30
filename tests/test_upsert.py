468d51314adac193e63b3a6ef9d67f0d43501e9b
Simon Willison <swillison@gmail.com>
2019-12-29 21:31:03 -0800

    test_upsert_compound_primary_key

--- tests/test_upsert.py
+++ tests/test_upsert.py
@@ -14,3 +14,13 @@ def test_upsert_all(fresh_db):
         {"id": 2, "name": "Nixie", "age": 5},
     ] == list(table.rows)
     assert 2 == table.last_pk
+
+
+def test_upsert_compound_primary_key(fresh_db):
+    table = fresh_db["table"]
+    table.upsert_all([{"species": "dog", "id": 1, "name": "Cleo", "age": 4}, {"species": "cat", "id": 1, "name": "Catbag"}], pk=("species", "id"))
+    table.upsert_all([{"species": "dog", "id": 1, "age": 5}], pk=("species", "id"))
+    assert [
+        {"species": "dog", "id": 1, "name": "Cleo", "age": 5},
+        {"species": "cat", "id": 1, "name": "Catbag", "age": None},
+    ] == list(table.rows)
