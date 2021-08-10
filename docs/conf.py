6ad3ce414cfba4c1947a9b5f201f416ecad04ce3
Simon Willison <swillison@gmail.com>
2021-08-10 10:43:32 -0700

    Leave this as it was

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ from subprocess import Popen, PIPE
 #
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath("../"))
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- General configuration ------------------------------------------------
