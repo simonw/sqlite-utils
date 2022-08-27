c5f8a2eb1a81a18b52825cc649112f71fe419b12
Forest Gregg <fgregg@users.noreply.github.com>
2022-08-27 10:45:03 -0400

    in extract code, check equality witH IS instead of = for nulls (#455)
    sqlite "IS" is equivalent to SQL "IS NOT DISTINCT FROM"
    
    close #423

--- sqlite_utils/db.py
+++ sqlite_utils/db.py
@@ -1791,7 +1791,7 @@ class Table(Queryable):
                 magic_lookup_column=magic_lookup_column,
                 lookup_table=table,
                 where=" AND ".join(
-                    "[{table}].[{column}] = [{lookup_table}].[{lookup_column}]".format(
+                    "[{table}].[{column}] IS [{lookup_table}].[{lookup_column}]".format(
                         table=self.name,
                         lookup_table=table,
                         column=column,
